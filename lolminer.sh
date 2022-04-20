eeae70040a4b33a3c93b875667969ff301e685ed
Anas Shaikh <itsanas1554@gmail.com>
2022-04-20 18:49:46 +0530

    Clear

--- lolminer.sh
+++ lolminer.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-clear
+123="clear"
+
+$123
 
 echo "Script made by Anas xD"
 
@@ -9,55 +11,34 @@ sleep 2
 echo "+ updating your apt pls wait" 
 sudo apt update -y
 
-clear
+$123
 
+echo "+ upgradeing your apt pls wait" 
 sudo apt upgrade -y
 
 clear
-
+echo "Downloading miner"
 wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.48/lolMiner_v1.48_Lin64.tar.gz
 
-clear
+$123
 
 tar -xzvf lolMiner_v1.48_Lin64.tar.gz
 
-clear
+$123
 
 cd 1.48/
 
-clear
-
+$123
+echo "Installing neofetch"
 apt install neofetch -y
 
-clear
+$123
 
 neofetch
 
 sleep 5
 
-clear
-
-cname=$( awk -F: '/model name/ {name=$2} END {print name}' /proc/cpuinfo | sed 's/^[ \t]*//;s/[ \t]*$//' )
-cores=$( awk -F: '/model name/ {core++} END {print core}' /proc/cpuinfo )
-freq=$( awk -F: '/cpu MHz/ {freq=$2} END {print freq}' /proc/cpuinfo | sed 's/^[ \t]*//;s/[ \t]*$//' )
-tram=$( free -m | awk '/Mem/ {print $2}' )
-uram=$( free -m | awk '/Mem/ {print $3}' )
-swap=$( free -m | awk '/Swap/ {print $2}' )
-uswap=$( free -m | awk '/Swap/ {print $3}' )
-up=$( awk '{a=$1/86400;b=($1%86400)/3600;c=($1%3600)/60} {printf("%d days, %d hour %d min\n",a,b,c)}' /proc/uptime )
-load=$( w | head -1 | awk -F'load average:' '{print $2}' | sed 's/^[ \t]*//;s/[ \t]*$//' )
-opsy=$( get_opsy )
-arch=$( uname -m )
-lbit=$( getconf LONG_BIT )
-kern=$( uname -r )
-disk_size1=($( LANG=C df -hPl | grep -wvE '\-|none|tmpfs|devtmpfs|by-uuid|chroot|Filesystem|udev|docker' | awk '{print $2}' ))
-disk_size2=($( LANG=C df -hPl | grep -wvE '\-|none|tmpfs|devtmpfs|by-uuid|chroot|Filesystem|udev|docker' | awk '{print $3}' ))
-disk_total_size=$( calc_disk "${disk_size1[@]}" )
-disk_used_size=$( calc_disk "${disk_size2[@]}" )
-
-sleep 5
-
-clear
+$123
 
 echo "Give miner a number / name"
 
